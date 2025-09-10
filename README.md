16c549311a53d538eb91ac1ba73d98fa98b9015a
Y46ne <yassine.belaarous@etu.univ-orleans.fr>
2025-09-10 15:12:04 +0200

    Tp 2 finis, voir README pour résumé

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ### Belaarous Yassine
 
 ### oublie de faire le readme dans la premiere séance
+
+TP2 : 
+
+* Installation de flask-alchemy
+
+* Modification des fichiers config.py et app.py afin de supporter le flask-alchemy
+
+* Création des tables en Python dans un fichier models.py
+
+* Création des tables sql avec flask loaddb
+
+* Ajout des données du fichier data.yml 
+
+* Test des commandes shells dans le shell pour interagir avec la base de données
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 #>>>import random string os
 #>>>"".join([random.choice(string.printable)for _in os.urandom(24)])
+import os
+
 SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
 ABOUT = "Bienvenue sur la page à propos de Flask !"
 INFO = "Ceci est une application de démonstration pour Flask faites par le goat Yassine B."
 CREDIT = "Developpeur : Belaarous Yassine"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
