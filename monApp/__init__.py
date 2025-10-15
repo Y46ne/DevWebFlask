bd064abda2fc96068fa5c03052d03cde8295b03b
Yassine Belaarous <yassine.belaarous@etu.univ-orleans.fr>
2025-10-15 20:38:31 +0200

    Delete monApp directory
    remplacement par celui fais a la maison, mauvais configuration sur pc a fait que je ne pouvais pas commit

--- monApp/__init__.py
+++
@@ -1,6 +0,0 @@
-from .app import app
-import monApp.views
-from .app import app, db
-import monApp.views
-import monApp.commands
-import monApp.models
--- monApp/tests/unit/test_models_user.py
+++
@@ -1,6 +0,0 @@
-from monApp.models import *
-
-def test_user_init(): 
-    user = User(Login = "Yasko", Password = "legarsdu41")
-    assert user.Login == "Yasko"
-    assert user.Password == "legarsdu41"    
